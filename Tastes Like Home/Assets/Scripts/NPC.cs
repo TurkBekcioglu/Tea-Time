b5c8114473d8de4160265961e0ea0a2ad14ef594
RogerChen98 <chen.rog@husky.neu.edu>
2019-01-27 09:41:14 -0500

    More art merged

---
+++ Tastes Like Home/Assets/Scripts/NPC.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+  public int race;
+  public int color;
+
+  public Character()
+  {
+    race = Random.Range(0, 4);
+    color = Random.Range(0, 4);
+  }
+}
